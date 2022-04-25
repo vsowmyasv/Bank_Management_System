638db3808ce0e4145c391422558b6b792162da2c
vsowmyasv <vsowmyasv@gmail.com>
2022-04-25 12:12:59 +0530

    -

--- src/DBController/DBHandler.java
+++ src/DBController/DBHandler.java
@@ -8,25 +8,20 @@ public class DBHandler {
     //function to check if the input credentials are valid
     public static void checkIfLoginExists(ResultSet resultSet)
     {
-        int check = 0;
         try
         {
             if(resultSet.next()) // checking if the query returns atleast 1 row
             {
-                check = 1;
+                Util.printToConsole("Logged in successfully");
             }
             else
             {
-                check = 0;
+                Util.printToConsole("Invalid Login");
             }
         }
         catch(SQLException e)
         {
             System.out.println(e);
         }
-        if(check == 1)
-        Util.printToConsole("Logged in successfully");
-        else
-        Util.printToConsole("Invalid Login");
     }
 }
